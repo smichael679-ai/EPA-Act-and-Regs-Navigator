cd859a60e75dea62e6f44d845b36a861590d33f7
smichael679-ai <smichael679@gmail.com>
2025-08-28 12:26:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -31,37 +31,4 @@ self.addEventListener('fetch', event => {
                 return fetch(event.request); // Fetch from network
             })
     );
-});const CACHE_NAME = 'epa-navigator-cache-v1';
-// Add the paths to your files here. 
-// The path must be relative to the root of your website.
-// For GitHub pages, if your repo is "epa-navigator", the paths will be "/epa-navigator/", "/epa-navigator/index.html", etc.
-// You will need to update this list after you know your repository name.
-const urlsToCache = [epa-navigator/index.html
-    './',
-    './index.html',
-    './manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response; // Serve from cache
-                }
-                return fetch(event.request); // Fetch from network
-            })
-    );
 });
